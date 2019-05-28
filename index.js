b766b1062b8cff4c566e1f98ce254435e27babf0
Julie Jonak <juliejonak10@gmail.com>
2019-05-28 16:25:53 -0700

    Updates

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const { withUiHook, htm } = require('@zeit/integration-utils')
 const twilio = require('twilio');
 
-const client = new twilio(store.AccountID, store.AuthTok);
-
-const store = {
+const storeInfo = {
     AccountID: '',
     AuthTok: '',
     Phone: '',
@@ -17,32 +15,41 @@ module.exports = withUiHook(async ({payload}) => {
     const {clientState, action} = payload;
 
     if (action === 'submit'){
-        store.AccountID = clientState.AccountID;
-        store.AuthTok = clientState.AuthTok;
-        store.Phone = clientState.Phone;
-        store.Message = clientState.Message;
+        storeInfo.AccountID = clientState.AccountID;
+        storeInfo.AuthTok = clientState.AuthTok;
+        storeInfo.Phone = clientState.Phone;
+        storeInfo.Message = clientState.Message;
+
+        const client = new twilio(storeInfo.AccountID, storeInfo.AuthTok);
 
         client.messages.create({
-            body: store.Message,
-            to: store.Phone,
+            body: storeInfo.Message,
+            to: storeInfo.Phone,
             from: '+19412567546'
           })
           .then((response) => {
-            store.MessageSent = true
+            storeInfo.MessageSent = true
           });
     }
 
-    if (store.MessageSent === true) {
+    if (storeInfo.MessageSent === true) {
         messageContainer = `<Container><h3>Successfully sent!</h3></Container>`
     }
 
     return htm`
         <Page>
             <Container>
-                <Input label="AccountID" type='password' name="AccountID" value=${store.AccountID} />
-                <Input label="AuthTok" type='password'  name="AuthTok" value=${store.AuthTok} />
-                <Input label="Phone" name="Phone" value=${store.Phone} />
-                <Input label="Message" name="Message" value=${store.Message} />
+                <Input label="AccountID" type='password' name="AccountID" value=${storeInfo.AccountID} />
+                <Input label="AuthTok" type='password'  name="AuthTok" value=${storeInfo.AuthTok} />
+                <Input label="Phone" name="Phone" value=${storeInfo.Phone} />
+                <Input label="Message" name="Message" value=${storeInfo.Message} />
+            </Container>
+
+            <Container>
+                <h4>Account ID: ${storeInfo.AccountID}</h4>
+                <h4>Auth: ${storeInfo.AuthTok}</h4>
+                <h4>Number: ${storeInfo.Phone}</h4>
+                <h4>Message: ${storeInfo.Message}</h4>
             </Container>
 
             <Container>
