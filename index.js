bca2d0f2a377cebedba1109b3294aaf581f3c7a9
Julie Jonak <juliejonak10@gmail.com>
2019-05-28 16:16:38 -0700

    Attempts to add Twilio messaging

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const { withUiHook, htm } = require('@zeit/integration-utils')
+const twilio = require('twilio');
+
+const client = new twilio(store.AccountID, store.AuthTok);
 
 const store = {
     AccountID: '',
     AuthTok: '',
     Phone: '',
-    Message: ''
+    Message: '',
+    MessageSent: false
 }
+
+messageContainer = `<Container><h3>Not yet sent</h3></Container>`
 
 module.exports = withUiHook(async ({payload}) => {
     const {clientState, action} = payload;
@@ -15,13 +21,26 @@ module.exports = withUiHook(async ({payload}) => {
         store.AuthTok = clientState.AuthTok;
         store.Phone = clientState.Phone;
         store.Message = clientState.Message;
+
+        client.messages.create({
+            body: store.Message,
+            to: store.Phone,
+            from: '+19412567546'
+          })
+          .then((response) => {
+            store.MessageSent = true
+          });
+    }
+
+    if (store.MessageSent === true) {
+        messageContainer = `<Container><h3>Successfully sent!</h3></Container>`
     }
 
     return htm`
         <Page>
             <Container>
-                <Input label="AccountID" name="AccountID" value=${store.AccountID} />
-                <Input label="AuthTok" name="AuthTok" value=${store.AuthTok} />
+                <Input label="AccountID" type='password' name="AccountID" value=${store.AccountID} />
+                <Input label="AuthTok" type='password'  name="AuthTok" value=${store.AuthTok} />
                 <Input label="Phone" name="Phone" value=${store.Phone} />
                 <Input label="Message" name="Message" value=${store.Message} />
             </Container>
@@ -29,6 +48,9 @@ module.exports = withUiHook(async ({payload}) => {
             <Container>
                 <Button action="submit">Submit</Button>
             </Container>
+
+            ${messageContainer}
+
         </Page>
     `
 });
