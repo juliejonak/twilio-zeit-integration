81dd9c5373d97729698a1675532c6082b109af44
Julie Jonak <juliejonak10@gmail.com>
2019-05-28 14:34:57 -0700

    Tests a basic form

---
+++ test-form.js
@@ -0,0 +1,34 @@
+const { withUiHook, htm } = require('@zeit/integration-utils')
+
+const store = {
+    AID: '',
+    AuthTok: '',
+    Phone: '',
+    Message: ''
+}
+
+module.exports = withUiHook(async ({payload}) => {
+    const {clientState, action} = payload;
+
+    if (action === 'submit'){
+        store.AID = clientState.AID;
+        store.AuthTok = clientState.AuthTok;
+        store.Phone = clientState.Phone;
+        store.Message = clientState.Message;
+    }
+
+    return htm`
+        <Page>
+            <Container>
+                <Input label="AID" name="AID" value=${store.AID} />
+                <Input label="AuthTok" name="AuthTok" value=${store.AuthTok} />
+                <Input label="Phone" name="Phone" type='password' value=${store.Phone} />
+                <Input label="Message" name="Message" value=${store.Message} />
+            </Container>
+
+            <Container>
+                <Button action="submit">Submit</Button>
+            </Container>
+        </Page>
+    `
+});
