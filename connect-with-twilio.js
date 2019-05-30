4b0b2a9f5931740a6cb0a46a06facbbb976685e5
Julie Jonak <juliejonak10@gmail.com>
2019-05-29 18:54:21 -0700

    Adds new code and method for possibly authenticating Twilio

---
+++ connect-with-twilio.js
@@ -0,0 +1,31 @@
+const { parse: parseUrl } = require('url');
+const cookie = require('cookie');
+
+const { TWILIO_APP_SID } = process.env;
+
+// Possible other redirect url: `https://www.twilio.com/authorize?client_id=${TWILIO_APP_SID}&state=${state}`
+
+
+module.exports = (req, res) => {
+    const {query} = parseUrl(req.url, true);
+    if (!query.next) {
+        res.writeHead(403);
+        res.end('Query param next is required');
+        return;
+    }
+
+    const state = `state_${Math.random()}`;
+    const redirectUrl = `https://www.twilio.com/authorize/${TWILIO_APP_SID}`
+    const context = {next: query.next, state};
+
+    res.writeHead(302, {
+        Location: redirectUrl,
+        'Set-Cookie': cookie.serialize('my-addon-context', JSON.stringify(context), {path: '/'})
+    });
+    res.end('Redirecting...');
+
+    // When successfully approved, Twilio returns the user back to the specified redirect URL with their account SID as a query param - we need to pull that out and add it to the metadata store on Zeit ( const Twilio_Acct_SID = req.query.AccountSid)
+
+    // Make Twilio requests with the user's SID and my own Twilio Auth Token
+
+}
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { withUiHook, htm } = require('@zeit/integration-utils')
 const twilio = require('twilio');
 
+const { TWILIO_SID, TWILIO_AUTH_TOK } = process.env;
+
+
 const storeInfo = {
     Phone: '',
     Message: '',
@@ -12,7 +15,7 @@ messageContainer = `<Container><h3>Not yet sent</h3></Container>`
 module.exports = withUiHook(async ({payload, zeitClient}) => {
     const twilioAuth = await zeitClient.getMetadata();
     const {clientState, action} = payload;
-    const client = new twilio(twilioAuth.AccountID, twilioAuth.AuthTok);
+    const client = new twilio(`${TWILIO_SID}`, `${TWILIO_AUTH_TOK}`);
 
     if (action === 'set'){
         twilioAuth.AccountID = clientState.AccountID;
@@ -28,7 +31,7 @@ module.exports = withUiHook(async ({payload, zeitClient}) => {
         client.messages.create({
             body: `${storeInfo.Message}`,
             to: `${storeInfo.Phone}`,
-            from: '+19412567546'
+            from: '+19412567546' // Change this to user's Twilio phone number
           })
           .then((response) => {
             console.log(response)
@@ -40,31 +43,38 @@ module.exports = withUiHook(async ({payload, zeitClient}) => {
         messageContainer = `<Container><h3>Successfully sent!</h3></Container>`
     }
 
+    // if res is error=unauthorized_client, Twilio declined access
+
     return htm`
         <Page>
-            <Container>
+
+            <Link href="https://www.twilio.com/authorize/CNe6cf47e469390206f80becb70d94774f">Authorize Twilio</Link>
+            
+            <Box>
                 <Input label="AccountID" type='password' name="AccountID" value=${twilioAuth.AccountID || ''} />
                 <Input label="AuthTok" type='password'  name="AuthTok" value=${twilioAuth.AuthTok || ''} />
-            </Container>
+            </Box>
 
-            <Container>
+            <Box>
                 <Input label="Phone" name="Phone" value=${storeInfo.Phone} />
                 <Input label="Message" name="Message" value=${storeInfo.Message} />
-            </Container>
+            </Box>
 
-            <Container>
+            <Box>
                 <P>Account ID: ${twilioAuth.AccountID}</P>
                 <P>Auth: ${twilioAuth.AuthTok}</P>
                 <P>Number: ${storeInfo.Phone}</P>
                 <P>Message: ${storeInfo.Message}</P>
-            </Container>
+            </Box>
 
-            <Container>
+            <Box>
                 <Button action="set">Set</Button>
                 <Button action="send">Send!</Button>
-            </Container>
+            </Box>
 
-            ${messageContainer}
+            <Box>
+                ${messageContainer}
+            </Box>
 
         </Page>
     `
