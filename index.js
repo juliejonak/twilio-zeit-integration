8d8bc223ff91beff4fb1f6053c5c14e6587c730c
Brandon G-Home <brandon.gardner2@outlook.com>
2019-05-31 14:48:26 -0500

    Current working code

--- index.js
+++ index.js
@@ -13,29 +13,34 @@ const AuthorizeView = require("./views/Authorize");
 module.exports = withUiHook(async ({ payload, zeitClient }) => {
   const metadata = await zeitClient.getMetadata();
   const { action, query } = payload;
-  switch (action) {
-    case "disconnect":
-      await disconnect(zeitClient, metadata);
-      break;
-    case "send-message":
+
+  try {
+    switch (action) {
+      case "disconnect":
+        await disconnect(zeitClient, metadata);
+        break;
+      case "send-message":
+        await sendMsg(metadata.userTwilioSID, metadata.twilioAuth);
+        return InfoView(metadata);
+    }
+
+    if (query.AccountSid) {
+      metadata.userTwilioSID = query.AccountSid;
+      metadata.twilioAuth = TWILIO_AUTH_TOK;
+      await zeitClient.setMetadata(metadata);
+
+      return MessageView(metadata);
+    }
+
+    if (metadata.userTwilioSID && metadata.twilioAuth) {
       await sendMsg(metadata.userTwilioSID, metadata.twilioAuth);
-      return InfoView(metadata);
+    }
+
+    // if res is error=unauthorized_client, Twilio declined access
+    return AuthorizeView(payload);
+  } catch (error) {
+    console.log(error);
   }
-
-  if (query.AccountSid) {
-    metadata.userTwilioSID = query.AccountSid;
-    metadata.twilioAuth = TWILIO_AUTH_TOK;
-    await zeitClient.setMetadata(metadata);
-
-    return MessageView(metadata);
-  }
-
-  if (metadata.userTwilioSID && metadata.twilioAuth) {
-    await sendMsg(metadata.userTwilioSID, metadata.twilioAuth);
-  }
-
-  // if res is error=unauthorized_client, Twilio declined access
-  return AuthorizeView(payload);
 });
 
 // withUiHook payload looks like:
