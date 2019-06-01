022e4dc41933c66d575944b741a99303c6750eef
Julie Jonak <juliejonak10@gmail.com>
2019-06-01 00:48:07 -0700

    Adds info element for showing saved metadata

--- views/Info.js
+++ views/Info.js
@@ -1,1 +1,10 @@
 // An element that returns the metadata once successfully stored to the meta store to ensure it matches the expected result
+
+const { htm } = require('@zeit/integration-utils');
+
+module.exports = metadata => htm`
+    <Page>
+        <P>Saved SID: ${metadata.userTwilioSID}</P>
+        <P>Saved Auth Token: ${metadata.twilioAuth}</P>
+    </Page>
+`;
