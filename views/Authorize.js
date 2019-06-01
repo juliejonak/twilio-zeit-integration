ce8ef54fbdff521b06145a009c292fc9515db921
Julie Jonak <juliejonak10@gmail.com>
2019-06-01 00:50:47 -0700

    Adds authorization element for using Twilio Connect

--- views/Authorize.js
+++ views/Authorize.js
@@ -1,1 +1,15 @@
 // The authorize button returned when a user needs to initially connect to Twilio
+
+const { htm } = require('@zeit/integration-utils');
+
+module.exports = payload => {
+    const connectUrl = `https://twilio-zeit-integration.juliejonak.now.sh/connect-with-twilio?next=${encodeURIComponent(payload.installationUrl)}`
+
+    return htm`
+        <Page>
+            <Button>
+                <Link href=${connectUrl}>Authorize Twilio</Link>
+            </Button>
+        </Page>
+    `;
+};
