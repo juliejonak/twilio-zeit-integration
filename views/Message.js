c01b21ae5f998c98cdbfd15e0fce530517acadba
Julie Jonak <juliejonak10@gmail.com>
2019-06-01 00:47:17 -0700

    Adds send message component for future reuse

--- views/Message.js
+++ views/Message.js
@@ -1,1 +1,12 @@
 // An element that returns the metadata saved SID and Auth, plus a button with which to test sending a message from that user's Twilio account
+
+const { htm } = require('@zeit/integration-utils');
+
+module.exports = metadata => htm`
+    <Page>
+        <P>SID: ${metadata.userTwilioSID}</P>
+        <P>Auth Token: ${metadata.twilioAuth}</P>
+
+        <Button action='send-message'>Send a message!</Button>
+    </Page>
+`;
