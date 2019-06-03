51e7eea21ab272714114c2e2778d17bb8fc72cca
Julie Jonak <juliejonak10@gmail.com>
2019-06-02 17:53:55 -0700

    Capitalizes the returned status

--- views/Texts.js
+++ views/Texts.js
@@ -9,22 +9,26 @@ module.exports = messageList => {
             <Box display="flex" marginTop="1em" marginBottom="1em">
 
                 <Box display="flex" alignItems="baseline" marginRight="4em" marginLeft="1em">
-                    <B>Sent to:</B><P>${m.sentTo}</P>
+                    <Box marginRight="1em"><B>Sent to:</B></Box>
+                    <Box><P>${m.sentTo}</P></Box>
                 </Box>
 
                 <Box display="flex" alignItems="baseline" width="75%">
-                    <B>Message:</B><P>${m.message}</P>
+                    <Box marginRight="1em"><B>Message:</B></Box>
+                    <Box><P>${m.message}</P></Box>
                 </Box>
             </Box>
 
             <Box display="flex">
 
                 <Box display="flex" alignItems="baseline" marginRight="4em" marginLeft="1em">
-                    <B>Date:</B><P>${format(m.date, 'ddd MMMM D YYYY [at] H[:]mm A')}</P>
+                    <Box marginRight="1em"><B>Date:</B></Box>
+                    <Box><P>${format(m.date, 'ddd MMMM D YYYY [at] H[:]mm A')}</P></Box>
                 </Box>
                 
                 <Box display="flex" alignItems="baseline" >
-                    <B>Status:</B><P>${m.status}</P>
+                   <Box marginRight="1em"><B>Status:</B></Box>
+                   <Box><P>${m.status.charAt(0).toUpperCase() + m.status.slice(1)}</P></Box>
                 </Box>
             </Box>
         </Fieldset>`
